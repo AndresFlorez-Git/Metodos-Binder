56425c5b72729e8be1ae17790307cd6b9acbfd88
Default user <af.florez12@uniandes.edu.co>
2019-07-02 17:38:25 +0000

    Linspace completo

--- S5C1/S5C1Deriv.cpp
+++ S5C1/S5C1Deriv.cpp
@@ -1,19 +1,30 @@
 #include<iostream>
 #include<math.h>
 
-double linspace(int, int, int)
-double derivada(double x)
+
+//double derivada(double x)
     
 int main(){
-
+   double fina;
+   double inicio;
+   int puntos;
+   inicio = 0;
+   fina = 10;
+   puntos = 10;
     
-    return 0
+    
+   double espacio[puntos];
+   double delta = (fina - inicio)/puntos ;
+   
+    
+    
+   for(int i=0; i<puntos;i++){
+      espacio[i] = inicio +i*delta;
+      std:: cout<<espacio[i]<<std:: endl;
+      
+   }
+    
+    return 0;
 }
 
-double linspace(double inicio, double fina, double puntos){
- double delta;
- double espacio[puntos]
- delta = (fina - inicio)/puntos ;
- for(int i=0; i<puntos)
-    
-}
+
