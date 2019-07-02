794dfc7f4acf78824542fc4d373af0322c9627a8
Default user <af.florez12@uniandes.edu.co>
2019-07-02 16:50:46 +0000

    Incompleto

---
+++ S5C1/S5C1Deriv.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<math.h>
+
+double linspace(int, int, int)
+double derivada(double x)
+    
+int main(){
+
+    
+    return 0
+}
+
+double linspace(double inicio, double fina, double puntos){
+ double delta;
+ double espacio[puntos]
+ delta = (fina - inicio)/puntos ;
+ for(int i=0; i<puntos)
+    
+}
