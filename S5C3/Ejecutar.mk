a29f613d40a445e578510de7cea6975293e0baa8
Default user <af.florez12@uniandes.edu.co>
2019-07-05 17:52:26 +0000

    Rk segundo orden realizado

--- S5C3/Ejecutar.mk
+++
@@ -1,7 +0,0 @@
-plotDatos.py: datos.dat FlorezAndres_S5C3_ODEs.cpp
-	python plotDatos.py
-	rm datos.dat 
-datos.dat:
-	g++ FlorezAndres_S5C3_ODEs.cpp
-	./a.out> datos.dat
-    
---
+++ S5C3/FlorezAndres_S5C3.mk
@@ -0,0 +1,7 @@
+FlorezAndres_S5C3_plots.py: datos.dat FlorezAndres_S5C3_ODEs.cpp
+	python FlorezAndres_S5C3_plots.py
+	rm datos.dat 
+datos.dat:
+	g++ FlorezAndres_S5C3_ODEs.cpp
+	./a.out> datos.dat
+    
