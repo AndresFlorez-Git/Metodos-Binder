765041c6d95dba5a89e5f02338cc75525423c2df
Default user <af.florez12@uniandes.edu.co>
2019-07-09 17:55:28 +0000

    Caso 1 resuelto

--- S6C2/FlorezAndres_S6C2.cpp
+++ S6C2/FlorezAndres_S6C2.cpp
@@ -13,12 +13,17 @@ int main()
     double t_finit = 0.1;
     int puntos = Longitud/dx;
     double A0 = 0.01;
-    
+    double dt = (dx/c)*0.25;
+    int puntosTemporales = (t_finit-t_init)/dt;
       
     
     /* Declaración de arreglos*/
     double x[puntos];
     double amplitud[puntos];
+    
+    double pasado[puntos];
+    double presente[puntos];
+    double futuro[puntos];
     
    
     
@@ -27,17 +32,18 @@ int main()
     int caso = 1;
     
     //Caso 1:tome condiciones de frontera fijas
-    if(caso = 1)
+    if(caso == 1)
     {
      x[0] = 0;
      amplitud[puntos/2] = A0;
      amplitud[0] = 0;
      amplitud[puntos-1] = 0;
-     
+     presente[0] = 0;
+     futuro[0] = 0;
     }
     
     //Caso 2:Repita lo anterior para un extremo fijo y un extremo libre
-    if(caso = 2)
+    if(caso == 2)
     {
      amplitud[puntos/2] = A0;
      amplitud[0] = 0;
@@ -65,13 +71,49 @@ int main()
             amplitud[i] = recta(-2*A0/Longitud, x[i],2*A0);
         }
         
-        std::cout<<x[i]<<","<<amplitud[i]<< std::endl;
+        pasado[i] = amplitud[i];
+        std::cout<<x[i]<<",";//<<amplitud[i]<< std::endl;
     }
     
+    std:: cout<<std::endl;
+    /* Evolución del sistema */
     
     
+    // Paso inmediato al inicio
+    for(int i = 1; i<= puntos; i++)
+    {
+       presente[i] = ((c*c*dt*dt)/(dx*dx)) * (pasado[i+1] + pasado[i-1] - 2*pasado[i]) + 2*pasado[i];
+    }
     
- 
+    // Construimos el futuro
+    int cto = 0;
+    for (int t = 0; t<= puntosTemporales; t++)
+    {
+        
+         if(cto == 0 || cto % 5 == 0)
+        {
+             for (int i = 0; i<=puntos;i++)
+             {
+              std:: cout<<presente[i]<<","; 
+             }
+         std:: cout<<std::endl; 
+        }
+        
+        
+        for (int i = 1; i<=puntos;i++ )
+        {
+            futuro[i] = ((c*c*dt*dt)/(dx*dx)) * (presente[i+1] + presente[i-1] - 2*presente[i])- pasado[i] + 2*presente[i];
+        }
+        
+        for(int i = 0; i<=puntos; i++)
+        {
+            pasado[i] = presente[i];
+            presente[i]= futuro[i];
+        }
+        
+       cto = cto+1;
+    }
+    
     
     return 0;
 }
--- S6C2/FlorezAndres_S6C2_plots.py
+++ S6C2/FlorezAndres_S6C2_plots.py
@@ -7,7 +7,7 @@ data = np.genfromtxt("datos.dat",delimiter=",")
 
 
 plt.figure(0,figsize=(10,10))
-plt.plot(data[:,0],data[:,1], label='Sistema')
+plt.plot(data[0,0:len(data[0,:])-1],data[1,0:len(data[0,:])-1], label='Sistema')
 plt.title('Condicion inicial del Sistema')
 plt.ylabel('Amplitud')
 plt.xlabel('x en tiempo = 0')
@@ -16,3 +16,27 @@ plt.xlabel('x en tiempo = 0')
 plt.legend(loc = 0)
 plt.grid()
 plt.savefig('FlorezAndres.png')
+
+
+def plot_for_offset(i, data):
+    # Data for plotting
+    t = data[0,0:len(data[0,:])-1]
+    s = data[i,0:len(data[0,:])-1]
+
+    fig, ax = plt.subplots(figsize=(10,5))
+    ax.plot(t, s)
+    ax.grid()
+    ax.set(xlabel='X en el tiempo ={}'.format(i), ylabel='Amplitud',
+           title='Evolución del Sitema')
+
+    ax.set_ylim(-3, 3)
+
+    # Used to return the plot as an image rray
+    fig.canvas.draw()       # draw the canvas, cache the renderer
+    image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+    image  = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+    return image
+
+kwargs_write = {'fps':20.0, 'quantizer':'nq'}
+imageio.mimsave('./Evolucion_Sistema_resuelto.gif', [plot_for_offset(i, data) for i in range(1,len(data[0,:]+200))], fps=20)
