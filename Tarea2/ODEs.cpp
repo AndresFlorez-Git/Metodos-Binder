6febfbff5513b794ee9a08a793f78860d28c55d0
Default user <af.florez12@uniandes.edu.co>
2019-07-15 19:01:11 +0000

    Declaración de condiciones iniciales

---
+++ Tarea2/ODEs.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<mat.h>
+#include<fstream>
+using namespace std;
+
+double G = 6.674e-11; //Constante de gravitacion universal [N m**2/ kg**2]
+double UA = 149597870700; // unidad astronomica en metros
+
+double Euler();
+
+int main(){
+ 
+    /*Constantes iniciales del problema*/
+    double X0 = 0.1163;
+    double Y0 = 0.9772;
+    
+    double vX0 = -6.35;
+    double vY0 = 0.606;
+    
+    int orbitas = 20;
+    
+    double t_init = 0;
+    double t_finit = orbitas/20;
+    
+    
+ return 0;   
+}
+
+double Funcion()
+{
+    
+}
+
+double Euler()
+{
+    
+}
