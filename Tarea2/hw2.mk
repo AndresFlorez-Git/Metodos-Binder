1e9981f6d45c888e3cd2a7084dae37bf48c1917d
Default user <af.florez12@uniandes.edu.co>
2019-07-18 19:34:06 +0000

    mk completo

--- Tarea2/hw2.mk
+++ Tarea2/hw2.mk
@@ -1,16 +1,14 @@
-Resultados_hw2.tex: Fourier.py FFT2D.png ODEs.cpp Plots_hw2.py Posicion_Orbital.png Velocidad_Orbital.png Momentum_Angular.png Energia_Euler.png Energia_LeapFrog.png Energia_RK.png Euler1.dat Euler2.dat Euler3.dat LeapFrog1.dat LeapFrog2.dat LeapFrog3.dat RK1.dat RK2.dat RK3.dat
+Resultados_hw2.pdf: Resultados_hw2.tex FFT2D.png Posicion_Orbital.png Velocidad_Orbital.png Momentum_Angular.png Energia_Euler.png Energia_LeapFrog.png Energia_RK.png 
 	pdflatex Resultados_hw2.tex
 
 FFT2D.png: Fourier.py
 	python Fourier.py
 
-Posicion_Orbital.png Velocidad_Orbital.png Momentum_Angular.png Energia_Euler.png Energia_LeapFrog.png Energia_RK.png: Plots_hw2.py ODEs.cpp Euler1.dat Euler2.dat Euler3.dat LeapFrog1.dat LeapFrog2.dat LeapFrog3.dat RK1.dat RK2.dat RK3.dat
+Posicion_Orbital.png Velocidad_Orbital.png Momentum_Angular.png Energia_Euler.png Energia_LeapFrog.png Energia_RK.png: Plots_hw2.py Euler1.dat Euler2.dat Euler3.dat LeapFrog1.dat LeapFrog2.dat LeapFrog3.dat RK1.dat RK2.dat RK3.dat
 	python Plots_hw2.py
 
-Plots_hw2.py: ODEs.cpp Euler1.dat Euler2.dat Euler3.dat LeapFrog1.dat LeapFrog2.dat LeapFrog3.dat RK1.dat RK2.dat RK3.dat
-	python Plots_hw2.py
-
-Euler1.dat Euler2.dat Euler3.dat LeapFrog1.dat LeapFrog2.dat LeapFrog3.dat RK1.dat RK2.dat RK3.dat: ODEs.cpp
-	g++ ODEs.cpp
+Euler1.dat Euler2.dat Euler3.dat LeapFrog1.dat LeapFrog2.dat LeapFrog3.dat RK1.dat RK2.dat RK3.dat: a.out
 	./a.out
     
+a.out: ODEs.cpp
+	g++ ODEs.cpp
